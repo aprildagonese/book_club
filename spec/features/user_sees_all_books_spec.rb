6a368310131a0a01ddf92215c96ba5a5e95a32a8
April Dagonese <adagonese@gmail.com>
2019-01-30 13:01:14 -0700

    Adds book images to book.index and author.show, and adds tests

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 describe 'books_index' do
   it "user_can_see_all_books" do
     author_2 = Author.create(name: "Jane Doe")
-    book_1 = author_2.books.create(title: "Book 1 Title", length: 111, year: 1111)
-    book_2 = author_2.books.create(title: "Book 2 Title", length: 222, year: 2222)
+    book_1 = author_2.books.create(title: "Book 1 Title", length: 111, year: 1111, cover_image: "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg")
+    book_2 = author_2.books.create(title: "Book 2 Title", length: 222, year: 2222, cover_image: "http://bookriotcom.c.presscdn.com/wp-content/uploads/2014/08/HP_hc_new_2-e1407533769415.jpeg")
 
     visit "/books"
     # save_and_open_page
@@ -13,9 +13,12 @@ describe 'books_index' do
     expect(page).to have_content("Length: 111")
     expect(page).to have_content("Year: 1111")
     expect(page).to have_content("Author(s):\nJane Doe")
+    expect(page).to have_xpath("//img[contains(@src,'#{File.basename(book_1.cover_image)}')]")
+
     expect(page).to have_content("Book 2 Title")
     expect(page).to have_content("222")
     expect(page).to have_content("2222")
     expect(page).to have_content("Jane Doe")
+    expect(page).to have_xpath("//img[contains(@src,'#{File.basename(book_2.cover_image)}')]")
   end
 end
--- spec/features/user_sees_an_author_page_spec.rb
+++ spec/features/user_sees_an_author_page_spec.rb
@@ -4,8 +4,8 @@ describe 'author_show_page' do
   it "user_can_see_author_books_data" do
     author_1 = Author.create(name: "Jon Doe")
     author_2 = Author.create(name: "Jane Doe")
-    book_1 = author_1.books.create(title: "Book 1 Title", length: 111, year: 1111)
-    book_2 = Book.create(title: "Book 2 Title", length: 222, year: 2222, authors: [author_1, author_2])
+    book_1 = author_1.books.create(title: "Book 1 Title", length: 111, year: 1111, cover_image: "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg")
+    book_2 = Book.create(title: "Book 2 Title", length: 222, year: 2222, authors: [author_1, author_2], cover_image: "http://bookriotcom.c.presscdn.com/wp-content/uploads/2014/08/HP_hc_new_2-e1407533769415.jpeg")
 
     visit "/authors/#{author_1.id}"
 
@@ -15,9 +15,14 @@ describe 'author_show_page' do
     expect(page).to have_content("Title: #{book_1.title}")
     expect(page).to have_content("Length: #{book_1.length}")
     expect(page).to have_content("Year: #{book_1.year}")
+    expect(page).to have_xpath("//img[contains(@src,'#{File.basename(book_1.cover_image)}')]")
+
     expect(page).to have_content("Title: #{book_2.title}")
     expect(page).to have_content("Length: #{book_2.length}")
     expect(page).to have_content("Year: #{book_2.year}")
     expect(page).to have_content("Co-Author(s):\n#{author_2.name}")
+    expect(page).to have_xpath("//img[contains(@src,'#{File.basename(book_2.cover_image)}')]")
+
+
   end
 end
