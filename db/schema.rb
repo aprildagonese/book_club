c6c095670c9643626db31ddc7c0c14a6d33905b4
Rene Casco <rene.casco@icloud.com>
2019-01-31 15:57:23 -0700

    migration created to delete column author_id from books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130230036) do
+ActiveRecord::Schema.define(version: 20190131224930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20190130230036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cover_image"
-    t.integer "author_id"
   end
 
   create_table "reviews", force: :cascade do |t|
--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -26,4 +26,15 @@ describe 'books_index' do
       expect(page).to have_xpath("//img[contains(@src,'#{File.basename(book_2.cover_image)}')]")
     end
   end
+
+  it "user can see area showing statistics for all books" do
+    author = Author.create(name: "Jon Smith")
+    author_2 = Author.create(name: "Jane Doe")
+    book_1 = Book.create(title: "Book 1 Title", length: 111, year: 1111, cover_image: "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg", authors: [author_2])
+    book_2 = Book.create(title: "Book 2 Title", length: 222, year: 2222, cover_image: "http://bookriotcom.c.presscdn.com/wp-content/uploads/2014/08/HP_hc_new_2-e1407533769415.jpeg", authors: [author_2])
+
+    visit "/books"
+    # save_and_open_page
+
+  end
 end
