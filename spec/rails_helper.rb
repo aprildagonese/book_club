b6df64f8ecacd346f95929fa2cfa0e6874ede004
April Dagonese <adagonese@gmail.com>
2019-02-04 09:50:02 -0700

    Adds simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,10 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
