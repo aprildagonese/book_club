db87621e85841aceb47edb2ca5943b3630d34d3e
Rene Casco <rene.casco@icloud.com>
2019-02-04 15:08:47 -0700

    model and featute tests passing for user 11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
+    @users = User.all
   end
 
   def index
--- spec/features/user_sees_book_show_page_spec.rb
+++ spec/features/user_sees_book_show_page_spec.rb
@@ -47,5 +47,48 @@ describe 'book_show_page' do
     end
   end
 
+  it "user_can_see_statistics for book" do
+    author_1 = Author.create(name: "John Smith")
 
+    book_1 = Book.create(title: "Book 1 Title", length: 111, year: 1111, cover_image: "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg", authors: [author_1])
+
+    april = User.create(name: "April")
+    rene = User.create(name: "Rene")
+    ian = User.create(name: "Ian")
+    megan = User.create(name: "Megan")
+    tim = User.create(name: "Tim")
+    teresa = User.create(name: "Teresa")
+    trevor = User.create(name: "Trevor")
+    julia = User.create(name: "Julia")
+
+    review_1 = book_1.reviews.create(title: "Review 1", description: "I liked this book", user: april, rating: 4)
+    review_2 = book_1.reviews.create(title: "Review 2", description: "so so", user: rene, rating: 1)
+    review_3 = book_1.reviews.create(title: "Review 3", description: "Wow!", user: ian, rating: 4)
+    review_4 = book_1.reviews.create(title: "Review 4", description: "As good as it gets!", user: megan, rating: 5)
+    review_5 = book_1.reviews.create(title: "Review 5", description: "Mixed feelings", user: tim, rating:3)
+    review_6 = book_1.reviews.create(title: "Review 6", description: "dfdfgsgfdgsggs", user: teresa, rating: 2)
+    review_7 = book_1.reviews.create(title: "Review 7", description: "gsfgfgdsgsgfsgs", user: trevor, rating: 1)
+    review_8 = book_1.reviews.create(title: "Review 8", description: "sgd hdfhsghf thshh", user: julia, rating: 3)
+
+    visit book_path(book_1)
+
+    within ".statistics-footer" do
+      within ".statistics-column-1" do
+        expect(page).to have_content("Top reviews for this book:")
+        expect(page).to have_content("#{review_4.title} #{review_4.rating} #{review_4.user.name}")
+        expect(page).to have_content("#{review_1.title} #{review_1.rating} #{review_1.user.name}")
+        expect(page).to have_content("#{review_3.title} #{review_3.rating} #{review_3.user.name}")
+      end
+      within ".statistics-column-2" do
+        expect(page).to have_content("Lowest reviews for this book:")
+        expect(page).to have_content("#{review_2.title} #{review_2.rating} #{review_2.user.name}")
+        expect(page).to have_content("#{review_7.title} #{review_7.rating} #{review_7.user.name}")
+        expect(page).to have_content("#{review_6.title} #{review_6.rating} #{review_6.user.name}")
+      end
+      within ".statistics-column-3" do
+        expect(page).to have_content("Average Rating for this Book:")
+        expect(page).to have_content(book_1.average_rating)
+      end
+    end
+  end
 end
